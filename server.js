ccc9a4f0180598f080c7f0e5bab279ecc2dbc226
Shaurya Mathur <shaurya.mathur@abyeti.com>
2020-08-09 14:13:29 +0530

    Bug Fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const path = require('path');
 const cors = require('cors');
-const http = require('http');
+const https = require('https');
 const socketServer = require('socket.io');
 const {
     addForm,
@@ -47,7 +47,7 @@ mongoose.connect(db, {
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = socketServer.listen(server);
 
 
